1c8bfcd1d1a84dbbf48bac81b576c9662ff198a7
JoeNemo208 <85529296+JoeNemo208@users.noreply.github.com>
2021-07-09 17:10:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-console.log("Is it work");
+var bodys=document.querySelectorAll("*");
+    var bodyarr=[];
+    var strs=["jlib-fontSize-","jlib-color-"];
+    var findClass=function(cls){
+    return cls.split(' ');  
+    };
+    var calc=function(x,y){
+        console.log(x+" "+y);
+        var b=y.replace(x,"");
+        console.log(b);
+    return b;
+    };
+    
+    for(var i=0;i<bodys.length;i++){
+
+    bodyarr.push(findClass(bodys[i].className))
+    }
+      
+    for(var i=0;i<bodys.length;i++){
+  
+    if(bodyarr[i]===""){
+    continue;
+    }
+     
+    for(var j=0;j<bodyarr[i].length;j++){
+    
+    if(bodyarr[i][j].includes(strs[0])===true){
+         //console.log(bodys.length);
+     bodys[i].style.fontSize=calc(strs[0],(bodyarr[i])[j]) +"px";
+
+    }
+    if(bodyarr[i][j].includes(strs[1])===true){
+         //console.log(bodys.length);
+    switch(calc(strs[1],(bodyarr[i])[j])){
+    case "red":
+    bodys[i].style.color="rgb(255,0,0)";
+    break;
+    case "blue":
+    bodys[i].style.color="rgb(0,0,255)";
+    break;
+    case "black":
+    bodys[i].style.color="rgb(0,0,0)";
+    break;
+    case "green":
+    bodys[i].style.color="rgb(0,255,0)";
+    break;
+    case "yellow":
+    bodys[i].style.color="rgb(255,255,0)";
+    break;
+    case "purple":
+    bodys[i].style.color="rgb(150, 0, 150)";
+    break;
+    case "orange":
+    bodys[i].style.color="rgb(255,155,0)";
+    break;
+    case "white":
+    bodys[i].style.color="rgb(255,255,255)";
+    break;
+    case "dark-green":
+    bodys[i].style.color="rgb(0,140,0)";
+    break;
+    case "pink":
+    bodys[i].style.color="rgb(242, 0, 255)";
+    break;
+        
+    }
+     
+    }
+    
+    
+    }
+    }
+   
+    
+    
